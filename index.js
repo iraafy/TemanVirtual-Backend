641b58324fc6338b18a2eb5ed1649da300575532
iraafy <irafitriyani62@gmail.com>
2023-08-02 10:55:02 +0700

    update

--- index.js
+++ index.js
@@ -4,228 +4,266 @@ const port = 3001;
 const bodyParser = require("body-parser");
 const db = require("./connection");
 const response = require("./response");
+require("dotenv").config();
+const { createClient } = require("@supabase/supabase-js");
+
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseAnonKey = process.env.SUPABASE_ANON_KEY;
+const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
 // const argon2 = require("argon2");
 
 app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
-  response(200, "API v.1 Ready To Go", "SUCCESS", res);
+  response(200, "API Teman Virtual Ready To Use", "SUCCESS", res);
 });
 
-app.get("/users", (req, res) => {
-  const sql = "SELECT * FROM users";
-  db.query(sql, (err, fields) => {
-    response(200, fields, "Get all users", res);
-  });
+app.get("/users", async (req, res) => {
+  const { data, error } = await supabase.from("users").select("*");
+  if (error) {
+    return response(500, null, error.message, res);
+  }
+  return response(200, data, "Get all user success", res);
 });
 
-app.get("/quotes", (req, res) => {
-  const sql = "SELECT * FROM quotes";
-  db.query(sql, (err, fields) => {
-    response(200, fields, "Get all quotes", res);
-  });
+app.get("/quotes", async (req, res) => {
+  const { data, error } = await supabase.from("quotes").select("*");
+  if (error) {
+    return response(500, null, error.message, res);
+  }
+  return response(200, data, "Get all quotes success", res);
 });
 
-app.get("/journal", (req, res) => {
-  const sql = "SELECT * FROM journal";
-  db.query(sql, (err, fields) => {
-    response(200, fields, "Get all journal", res);
-  });
-});
-
-app.get("/artikel", (req, res) => {
-  const sql = "SELECT * FROM artikel";
-  db.query(sql, (err, fields) => {
-    response(200, fields, "Get all artikel", res);
-  });
-});
-
-app.get("/moods", (req, res) => {
-  const sql = "SELECT * FROM moods";
-  db.query(sql, (err, fields) => {
-    response(200, fields, "Get all moods", res);
-  });
-});
-
-app.get("/artikel/:id", (req, res) => {
-  const id = req.params.id;
-  const sql = `SELECT * FROM artikel WHERE id = ${id}`;
-  db.query(sql, (err, fields) => {
-    if (err) {
-      response(500, "Error", "Internal Server Error", res);
-    }
-    response(200, fields, "Get detail artikel", res);
-  });
-});
-
-app.get("/moods/:id", (req, res) => {
-  const id = req.params.id;
-  const sql = `SELECT * FROM moods WHERE id_user = ${id}`;
-  db.query(sql, (err, fields) => {
-    if (err) {
-      response(500, "Error", "Internal Server Error", res);
-    }
-    fields.sort((a, b) => new Date(b.timestamp) - new Date(a.timestamp));
-    response(200, fields, "Get detail moods", res);
-  });
-});
-
-app.get("/journal/:id", (req, res) => {
-  const id = req.params.id;
-  const sql = `SELECT * FROM journal WHERE id_user = ${id}`;
-  db.query(sql, (err, fields) => {
-    if (err) {
-      response(500, "Error", "Internal Server Error", res);
-    }
-    fields.sort((a, b) => new Date(b.timestamp) - new Date(a.timestamp));
-    response(200, fields, "Get detail journal", res);
-  });
-});
-
-app.get("/users/:id", (req, res) => {
-  const id = req.params.id;
-  const sql = `SELECT * FROM users WHERE id = ${id}`;
-  db.query(sql, (err, fields) => {
-    if (err) {
-      response(500, "Error", "Internal Server Error", res);
-    }
-    response(200, fields, "Get detail users", res);
-  });
+app.get("/article", async (req, res) => {
+  const { data, error } = await supabase.from("article").select("*");
+  if (error) {
+    return response(500, null, error.message, res);
+  }
+  return response(200, data, "Get all article success", res);
 });
 
 app.post("/register", async (req, res) => {
   const { name, email, password, friend, profile } = req.body;
+  try {
+    const { data, error } = await supabase
+      .from("users")
+      .insert([{ name, email, password, friend, profile }])
+      .select();
+    if (error) {
+      return response(500, null, "Internal Server Error", res);
+    }
 
-  const checkEmailQuery = `SELECT * FROM users WHERE email = '${email}'`;
-  db.query(checkEmailQuery, async (err, rows) => {
-    if (err) {
-      response(500, "Error", "Internal Server Error", res);
+    if (data.length === 1) {
+      const user = data[0];
+      const userData = {
+        isSuccess: "success",
+        id: user.id,
+      };
+      return response(200, userData, "Register successfully", res);
     } else {
-      if (rows.length > 0) {
-        response(400, "Error", "Email sudah tersedia, gunakan email lain", res);
-      } else {
-        try {
-          // const hashedPassword = await argon2.hash(password);
-          const hashedPassword = password;
-          const sql = `INSERT INTO users (name, email, password, friend, profile) VALUES ('${name}', '${email}', '${hashedPassword}', '${friend}', '${profile}')`;
-          db.query(sql, (err, fields) => {
-            if (err) {
-              response(500, "Error", "Internal Server Error", res);
-            } else {
-              const data = {
-                isSuccess: fields.affectedRows,
-                id: fields.insertId,
-              };
-              response(200, data, "Data berhasil ditambahkan", res);
-            }
-          });
-        } catch (error) {
-          response(500, "Error", "Internal Server Error", res);
-        }
-      }
+      return response(404, null, "User not found", res);
     }
-  });
+  } catch (error) {
+    console.error(error);
+    return response(500, null, "Internal Server Error", res);
+  }
 });
 
-app.post("/journal", (req, res) => {
-  const { id_user, title, content, timestamp } = req.body;
-  const sql = `INSERT INTO journal (id_user, title, content, timestamp) VALUES ('${id_user}', '${title}', '${content}', '${timestamp}')`;
-  db.query(sql, (err, fields) => {
-    if (err) {
-      response(500, "Error", "Internal Server Error", res);
-    }
-    if (fields.affectedRows) {
-      const data = {
-        isSuccess: fields.affectedRows,
-        id: fields.insertId,
-      };
-      response(200, data, "Data berhasil ditambahkan", res);
-    }
-  });
-});
-
-app.post("/moods", (req, res) => {
-  const { id_user, mood, reason, timestamp } = req.body;
-  const sql = `INSERT INTO moods (id_user, mood, reason, timestamp) VALUES ('${id_user}', '${mood}', '${reason}', '${timestamp}')`;
-  db.query(sql, (err, fields) => {
-    if (err) {
-      response(500, "Error", "Internal Server Error", res);
-    }
-    if (fields.affectedRows) {
-      const data = {
-        isSuccess: fields.affectedRows,
-        id: fields.insertId,
-      };
-      response(200, data, "Data berhasil ditambahkan", res);
-    }
-  });
-});
-
-app.put("/users", (req, res) => {
-  const { id, name, email, password, friend, profile } = req.body;
-  const sql = `UPDATE users SET name = '${name}', email = '${email}', password = '${password}', friend = '${friend}', profile = '${profile}' WHERE id = ${id}`;
-  db.query(sql, (err, fields) => {
-    if (err) {
-      response(500, "Error", "Internal Server Error", res);
-    }
-    if (fields.affectedRows) {
-      const data = {
-        isSuccess: fields.affectedRows,
-        message: fields.message,
-      };
-      response(200, data, "Data berhasil diupdate", res);
-    } else {
-      response(404, "Error", "Data tidak ditemukan", res);
-    }
-  });
-});
-
-app.delete("/users", (req, res) => {
-  const { id } = req.body;
-  const sql = `DELETE FROM users WHERE id = ${id}`;
-  db.query(sql, (err, fields) => {
-    if (err) {
-      response(500, "Error", "Internal Server Error", res);
-    }
-    if (fields.affectedRows) {
-      const data = {
-        isDeleted: fields.affectedRows,
-      };
-      response(200, data, "Data deleted successfully", res);
-    } else {
-      response(404, "Error", "Data tidak ditemukan", res);
-    }
-  });
-});
-
-app.post("/login", (req, res) => {
+app.post("/login", async (req, res) => {
   const { email, password } = req.body;
-  const sql = `SELECT * FROM users WHERE email = '${email}'`;
-  db.query(sql, async (err, rows) => {
-    if (err) {
-      response(500, "Error", "Internal Server Error", res);
-    } else {
-      if (rows.length === 1) {
-        const user = rows[0];
-        const match = true;
-        // const match = await argon2.verify(user.password, password);
-        if (match) {
-          const data = {
-            id: user.id,
-            name: user.name,
-            email: user.email,
-          };
-          response(200, data, "Login successfully", res);
-        } else {
-          response(401, "Error", "Invalid email/password", res);
-        }
-      } else {
-        response(404, "Error", "User not found", res);
-      }
+  try {
+    const { data, error } = await supabase
+      .from("users")
+      .select("*")
+      .eq("email", email);
+
+    if (error) {
+      return response(500, null, "Internal Server Error", res);
     }
-  });
+
+    if (data.length === 1) {
+      const user = data[0];
+      const isPasswordValid = user.password === password;
+      if (isPasswordValid) {
+        const userData = {
+          id: user.id,
+          name: user.name,
+          email: user.email,
+        };
+        return response(200, userData, "Login successfully", res);
+      } else {
+        return response(401, null, "Invalid email/password", res);
+      }
+    } else {
+      return response(404, null, "User not found", res);
+    }
+  } catch (error) {
+    console.error(error);
+    return response(500, null, "Internal Server Error", res);
+  }
+});
+
+app.get("/users/:id", async (req, res) => {
+  const id = req.params.id;
+  try {
+    const { data, error } = await supabase
+      .from("users")
+      .select("*")
+      .eq("id", id);
+
+    if (error) {
+      return response(500, null, "Internal Server Error", res);
+    }
+
+    if (data.length === 1) {
+      const userData = data[0];
+      return response(200, userData, "Get detail artikel", res);
+    } else {
+      return response(404, null, "Artikel not found", res);
+    }
+  } catch (error) {
+    console.error(error);
+    return response(500, null, "Internal Server Error", res);
+  }
+});
+
+app.get("/article/:id", async (req, res) => {
+  const id = req.params.id;
+  try {
+    const { data, error } = await supabase
+      .from("article")
+      .select("*")
+      .eq("id", id);
+
+    if (error) {
+      return response(500, null, "Internal Server Error", res);
+    }
+
+    if (data.length === 1) {
+      const artikelData = data[0];
+      return response(200, artikelData, "Get detail artikel", res);
+    } else {
+      return response(404, null, "Artikel not found", res);
+    }
+  } catch (error) {
+    console.error(error);
+    return response(500, null, "Internal Server Error", res);
+  }
+});
+
+app.get("/moods/:user_id", async (req, res) => {
+  const user_id = req.params.user_id;
+  try {
+    const { data, error } = await supabase
+      .from("moods")
+      .select("*")
+      .eq("user_id", user_id);
+
+    if (error) {
+      return response(500, null, "Internal Server Error", res);
+    }
+
+    if (data.length > 0) {
+      return response(200, data, "Get moods data successfully", res);
+    } else {
+      return response(
+        404,
+        null,
+        "No moods data found for the given user_id",
+        res
+      );
+    }
+  } catch (error) {
+    console.error(error);
+    return response(500, null, "Internal Server Error", res);
+  }
+});
+
+app.get("/journal/:user_id", async (req, res) => {
+  const user_id = req.params.user_id;
+  try {
+    const { data, error } = await supabase
+      .from("journal")
+      .select("*")
+      .eq("user_id", user_id);
+
+    if (error) {
+      return response(500, null, "Internal Server Error", res);
+    }
+
+    if (data.length > 0) {
+      return response(200, data, "Get journal data successfully", res);
+    } else {
+      return response(
+        404,
+        null,
+        "No journal data found for the given user_id",
+        res
+      );
+    }
+  } catch (error) {
+    console.error(error);
+    return response(500, null, "Internal Server Error", res);
+  }
+});
+
+app.post("/moods", async (req, res) => {
+  const { user_id, mood, reason, timestamp } = req.body;
+  try {
+    const { data, error } = await supabase
+      .from("moods")
+      .insert([{ user_id, mood, reason, timestamp }])
+      .select();
+    if (error) {
+      return response(500, null, "Internal Server Error", res);
+    }
+
+    if (data.length === 1) {
+      const moods = data[0];
+      const moodsData = {
+        isSuccess: "success",
+        id: moods.id,
+      };
+      return response(200, moodsData, "Data berhasil ditambahkan", res);
+    } else {
+      return response(404, null, "Moods not found", res);
+    }
+  } catch (error) {
+    console.error(error);
+    return response(500, null, "Internal Server Error", res);
+  }
+});
+
+app.post("/journal", async (req, res) => {
+  const { user_id, title, content, timestamp } = req.body;
+  try {
+    const { data, error } = await supabase
+      .from("journal")
+      .insert([{ user_id, title, content, timestamp }])
+      .select();
+    if (error) {
+      return response(500, null, "Internal Server Error", res);
+    }
+
+    if (data.length === 1) {
+      const journal = data[0];
+      const journalData = {
+        isSuccess: "success",
+        id: journal.id,
+      };
+      return response(200, journalData, "Data berhasil ditambahkan", res);
+    } else {
+      return response(404, null, "Journal not found", res);
+    }
+  } catch (error) {
+    console.error(error);
+    return response(500, null, "Internal Server Error", res);
+  }
 });
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
+
+module.exports = app;
